2f9134bec3480ddd65d78f8a1efabdd5559be5ff
sergeykham <sergeykham@gmail.com>
2018-09-29 22:06:52 +0300

    first version of tele bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+bot_token = os.environ['BOT_TOKEN']
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
